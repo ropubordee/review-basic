5b565a175aa41af1aee4e4fb4945639a5ea4437a
devpubordee <boszza1357009@gmail.com>
2025-08-26 02:45:31 +0700

    tags a style css

---
+++ javascript/app.js
@@ -0,0 +1,36 @@
+document.write("Pubordee.com");
+document.write("<p>Javascript เบี้องต้น</p>");
+
+// alert("สวัสดีครับ")
+console.log("Hello JavaScript");
+// console.error('เจอไวรัส');
+// console.warn("เจอผิดพลาด")
+
+// แปลงค่าเลข
+
+// let age = "20";
+
+// console.log(age);
+// console.log(typeof age);
+//
+
+// Array
+
+// let number = [1, 2, 3, 4, 5, 6];
+// number[1] = 200;
+// console.log(number);
+// console.log("ตัวแรก = ", number[0]);
+
+//
+
+// + - x %
+
+console.log('ผลบวก =',20+10);
+console.log('ผลลบ = ', 20-10);
+console.log('ผลคูณ = ',20*10);
+console.log('ผลหาร =', 20/10);
+console.log('ผลเศษ = ', 20%10)
+console.log('ผลยกกำลัง = ',20**2);
+
+
+
