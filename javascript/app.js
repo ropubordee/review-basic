c2ecb5fae8fb0d8a37de4276f96baa818584d354
devpubordee <boszza1357009@gmail.com>
2025-09-23 03:15:37 +0700

    function

--- javascript/app.js
+++ javascript/app.js
@@ -32,9 +32,7 @@ console.log("Hello JavaScript");
 // console.log('ผลเศษ = ', 20%10)
 // console.log('ผลยกกำลัง = ',20**2);
 
-
 // && , ||, !
-
 
 // let score = 70
 
@@ -44,11 +42,60 @@ console.log("Hello JavaScript");
 
 // if
 
-age = 10
+// age = 10
 
-if(age == 15){
-    console.log('คำนำหน้าเป็นนาย/นางสาว');
-    
-}
+// if(age == 15){
+//     console.log('คำนำหน้าเป็นนาย/นางสาว');
+
+// }
 //
 
+// function
+
+const plusNumber = (x) => {
+  console.log("ค่าที่ส่งมา = ", x);
+};
+const fullName = (fname, lname, city = "กรุงเทพมหานคร") => {
+  console.log("ชื่อจริง = ", fname, "นามสกุล = ", lname, "ที่อยู่ = ", city);
+};
+// const summation = (x, y) => {
+//   let total = x + y;
+//   console.log(`ผลรวม = ${total}`);
+// };
+
+const getComputerIP = () => {
+  return "127.0.0.1";
+};
+
+const getNumber = () => {
+  return 100 * 100;
+};
+const getMyaddress = () => {
+  let city = "ระยอง";
+  return city;
+};
+
+const setSalary = (salary) => {
+  let bonus = 1000;
+  return salary + bonus;
+};
+
+const summation = (x, y) => {
+  return x + y;
+};
+
+const getName = (name, city) => {
+  return name + " " + city;
+};
+
+fullName('pubordee','srisurach', 'มหาสารคาม')
+
+console.log(getName("ใจดี", "ระยอง"));
+
+
+
+let sum = summation(50, 100);
+console.log(`ผลรวมการบวกเลข =  ${sum}`);
+
+let total = setSalary(15000);
+console.log(`เงืนเดือน+โบนัส = ${total}`);
