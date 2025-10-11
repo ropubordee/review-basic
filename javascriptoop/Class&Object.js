700379b8de442e292cc8df52c0fa2f48968280e7
devpubordee <boszza1357009@gmail.com>
2025-10-12 03:57:25 +0700

    javascript oop

---
+++ javascriptoop/Class&Object.js
@@ -0,0 +1,19 @@
+class User {
+  name = "pubordee";
+  password = 1234;
+}
+const user1 = new User();
+user1.name = "jojo";
+user1.password = 7890;
+console.log(user1);
+
+const user2 = new User()
+user2.name='nut'
+user2.password=9876
+console.log(user2);
+
+const user3 = new User()
+
+console.log(user3);
+
+
